2d488c2747a8ff421628fb14da7862e69c1fbea5
SoniaAltamiranda <sonia.altamiranda10@gmail.com>
2023-07-31 17:52:24 -0300

    Agregando archivo txt

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
         <a className="flex title-font font-medium items-center md:justify-start justify-center text-[#8C7064]">
           {" "}
-          {/* Agregamos la clase de color aquí */}
+    
           <span className="ml-3 text-xl">
             <img
               src="/src/img/LOGO IRONIC.png"
